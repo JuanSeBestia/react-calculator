f6a6cc55276245807043a9051b2a58ffb2682614
JuanSe Dussan <Juan.Dussan@nordstrom.com>
2019-08-22 11:17:20 -0500

    Add const into function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-const isOperator = val => {
+export const Button = props => {
+  const isOperator = val => {
     return !isNaN(val) || val === "." || val === "=";
-}
-
-export const Button = props => (
-    <div className={`btn-wrapper ${
+  };
+  return (
+    <div
+      className={`btn-wrapper ${
         isOperator(props.children) ? null : "operator"
-    }`}
-    onClick={() => props.handleClick(props.children)}>
-        {props.children}
+      }`}
+      onClick={() => props.handleClick(props.children)}
+    >
+      {props.children}
     </div>
-)
+  );
+};
 
 export default Button;
